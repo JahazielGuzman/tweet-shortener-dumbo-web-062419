c511414a7450d12c12a52d192edfef87e3004247
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-15 17:56:00 -0400

    tweet shortener complete

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,44 @@
 # Write your code here.
+def word_substituter(tweet)
+	dictionary = {"hello" => 'hi',
+	"to" => '2',
+	"two" => '2',
+	"too" => '2',
+	"for" => '4',
+	"four" => '4',
+	"be" => 'b',
+	"you" => 'u',
+	"at" => "@", 
+	"and" => "&"}
+
+	tweet = tweet.split(" ")
+	tweet.each_with_index do |w, i|
+		if dictionary[w.downcase] != nil
+			tweet[i] = dictionary[w.downcase]
+		end
+	end
+
+	tweet.join(" ")
+end
+
+def bulk_tweet_shortener(tarr)
+	tarr.each do |tweet|
+		puts word_substituter(tweet)
+	end
+end
+
+def selective_tweet_shortener(tweet)
+	if tweet.length > 140
+		word_substituter(tweet)
+	else
+		tweet
+	end
+end
+
+def shortened_tweet_truncator(tweet)
+	if tweet.length > 140
+		tweet[0...140]
+	else
+		tweet
+	end
+end
